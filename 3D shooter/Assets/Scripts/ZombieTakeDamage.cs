1cfb675da723eabd618363250764e5f0fe56dbc6
Artisan899 <150586332+Artisan899@users.noreply.github.com>
2024-05-18 18:14:13 +0300

    Update ZombieTakeDamage.cs
    Переработка респавна врагов

--- 3D shooter/Assets/Scripts/ZombieTakeDamage.cs
+++ 3D shooter/Assets/Scripts/ZombieTakeDamage.cs
@@ -3,24 +3,50 @@ using UnityEngine;
 
 public class ZombieTakeDamage : NetworkBehaviour
 {
-    public float maxHealth = 40f; //здоровье зомби
+    public int maxHealth = 40; //здоровье зомби
     [SyncVar]
-    private float currentHealth;
+    private int currentHealth;
 
     private void Start()
     {
         currentHealth = maxHealth;
     }
 
-
-    public void CmdTakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
+        if (!isServer) return; // Если это не сервер, выходим
 
         currentHealth -= damage;
         if (currentHealth <= 0f)
         {
-            Destroy(gameObject);
-            SpawnEnemy.Instance.EnemyDied(this);
+
+              RpcRespawn();
+
         }
     }
+
+    [ClientRpc]
+    private void RpcRespawn()
+    {
+
+        Vector3[] respawnPositions = new Vector3[] // Создаем массив векторов с различными координатами для точек спавна 
+        {
+            new Vector3(-7, 2, -10),
+            new Vector3(10, 2, -18),
+            new Vector3(12, 2, 3),
+            new Vector3(7, 2, -10),
+            new Vector3(12, 2, 15),
+        };
+
+        int randomIndex = Random.Range(0, respawnPositions.Length); // Выбираем случайный индекс из массива
+
+        Vector3 respawnPosition = respawnPositions[randomIndex];   // Получаем случайный вектор из массива
+
+        transform.position = respawnPosition;  // Перемещаем НПС на выбранные координаты
+
+        currentHealth = maxHealth;  // Восстанавливаем здоровье после реса
+    }
+
+
+ 
 }
